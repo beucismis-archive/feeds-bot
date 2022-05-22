9e5a40bcf1c4a5b0a85d56b463d705188a1f9ba5
beucismis <beucismis@tutamail.com>
2022-05-23 02:07:10 +0300

    Update feeds-bot.py

--- feeds-bot.py
+++ feeds-bot.py
@@ -7,7 +7,7 @@ from src.main import Bot
 
 if __name__ == "__main__":
     if not os.path.exists("configs.json"):
-        print("I: Configs file not found!")
+        print("Configs file not found!")
         sys.exit(1)
 
     Bot().run()
